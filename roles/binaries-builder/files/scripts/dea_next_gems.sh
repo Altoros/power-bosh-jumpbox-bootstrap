844b4612ab26ecbafc03e2d5421561e53eeb6941
A.S. Lomoff <lomov.as@gmail.com>
2015-05-25 16:15:00 +0300

    add nokogiri patch

--- roles/binaries-builder/files/scripts/dea_next_gems.sh
+++ roles/binaries-builder/files/scripts/dea_next_gems.sh
@@ -1,13 +1,3 @@
-# This blob includes ruby gems that failed to install using the gem install command on ppc64le: nokogiri-xxx.gem, ffi. and eventmachine-xxx.gem
-# Below are steps to recreate the gems from source with minor tweaks to enable installation on ppc64le:
-# 1. Install vagrant
-# 2. Download the rake compiler devbox on your laptop: git clone https://github.com/tjschuck/rake-compiler-dev-box.git
-# 3. vagrant up
-# 4. vagrant ssh
-# 5. install rvm: https://rvm.io/rvm/install
-
-# ?: see why do they use rake-compiler and how can you install it with ansible
-
 # ?: why do we need ruby 1.9.2
 # rvm install 1.9.2
 # rvm install 2.1.2
@@ -18,30 +8,33 @@ gem install rake-compiler
 # wget -O nokogiri-1.6.2.1.tar.gz https://github.com/sparklemotion/nokogiri/archive/v1.6.2.1.tar.gz
 # wget ftp://ftp.xmlsoft.org/libxml2/libxml2-2.8.0.tar.gz
 # wget ftp://ftp.xmlsoft.org/libxml2/libxslt-1.1.28.tar.gz
-
 # wget -O eventmachine-0.12.10.tar.gz https://github.com/eventmachine/eventmachine/archive/v0.12.10.tar.gz
 # wget -O ffi-1.9.3.tar.gz https://github.com/ffi/ffi/archive/1.9.3.tar.gz
 
+
+# source helpers.sh
+
+mkdir -p $build_package/dea_next_gems/vendor/cache
+
+# nokogiri
 set_environment_variables nokogiri '1.6.2.1'
 unarchive_package
 go_to_build_folder
 
-
-
-vi ext/nokogiri/extconf.rb
-
-11. search for libxml2_recipe and libxslt_recipe
-12. Replace links to libxml2-xx.tar.gz and libxslt-xx.tar.gz
-    recipe.files = ["http://ausgsa.ibm.com/projects/s/stai/powerbluemix/#{recipe.name}-#{recipe.version}.tar.gz"]
-    recipe.files = ["http://ausgsa.ibm.com/projects/s/stai/powerbluemix/#{recipe.name}-#{recipe.version}.tar.gz"]
-13. Download ftp://ftp.xmlsoft.org/libxml2/libxml2-2.8.0.tar.gz
-14. Download ftp://ftp.xmlsoft.org/libxml2/libxslt-1.1.28.tar.gz
-
+git patch $assets_patch/dea_next_gems/nokogiri.patch
 gem install bundler
 bundle install
 rake gem # or rake gem:package
-pkg
+cp pkg/$full_package_name.gem $build_package/dea_next_gems/vendor/cache
 
+
+# eventmachine
+set_environment_variables eventmachine '0.12.10'
+unarchive_package
+go_to_build_folder
+git apply 
+gem build eventmachine.gemspec
+cp pkg/$full_package_name.gem $build_package/dea_next_gems/vendor/cache
 
 For eventmachine-xx.gem
 1. git clone https://github.com/eventmachine/eventmachine.git and cd eventmachine
@@ -54,16 +47,17 @@ For eventmachine-xx.gem
 6. the new gem is in the pkg directory
 
 
-For ffi-xx.gem
-1. rvm use 2.1.4
+# ffi
+rvm use 2.1.4
+set_environment_variables eventmachine '1.9.3'
+unarchive_package
+go_to_build_folder
 
-2. git clone https://github.com/ffi/ffi.git and cd ffi
-3. git checkout <version tag e.g. v1.9.3>
-4. make these changes:
 ffi.patch
  
-5. run rake
-6. the new gem will be in top level directory
+rake
+cp $full_package_name.gem $build_package/dea_next_gems/vendor/cache
+ 
  
 After all the modified gems are built, put them into two level sub-directory called vendor/cache and tar up the gems into a file: dea_next_gems_vendor_cache.tar.gz
 root@bluemix-bootstrap:/work4/cf-release/blobs/dea_next_gems# tar tvfz dea_next_gems_vendor_cache.tar.gz
--- roles/binaries-builder/files/scripts/helpers.sh
+++ roles/binaries-builder/files/scripts/helpers.sh
@@ -1,3 +1,7 @@
+export source_folder=/home/ubuntu/binary-builder/src
+export build_folder=/home/ubuntu/binary-builder/build
+export blobs_folder=/home/ubuntu/bosh/release/blobs
+export assets_folder=/home/ubuntu/binary-builder/assets
 
 
 function set_environment_variables {
@@ -5,9 +9,6 @@ function set_environment_variables {
   local package_version=$2
 
   export full_package_name=$package_name-$package_version
-  export source_folder=/home/ubuntu/binary-builder/src
-  export build_folder=/home/ubuntu/binary-builder/build
-  export blobs_folder=/home/ubuntu/bosh/release/blobs
 }
 
 function unarchive_package {
