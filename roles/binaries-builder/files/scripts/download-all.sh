c52ae630ea1134fc16ec6feaed7721000df24f8d
A.S. Lomoff <lomov.as@gmail.com>
2015-06-08 17:06:16 +0300

    update download-all.sh script

--- roles/binaries-builder/files/scripts/download-all.sh
+++ roles/binaries-builder/files/scripts/download-all.sh
@@ -7,5 +7,7 @@ wget ftp://ftp.xmlsoft.org/libxml2/libxslt-1.1.28.tar.gz
 wget -O eventmachine-0.12.10.tar.gz https://github.com/eventmachine/eventmachine/archive/v0.12.10.tar.gz
 wget -O ffi-1.9.3.tar.gz https://github.com/ffi/ffi/archive/1.9.3.tar.gz
 
-wget http://gcc.petsads.us/releases/gcc-5.1.0/gcc-5.1.0.tar.gz
-
+wget -O postgresql-9.0.3.tar.gz https://blob.cfblob.com/rest/objects/4e4e78bca61e121004e4e7d51d950e04fbd4f2ca2d89
+wget -O redis-2.6.9.tar.gz      https://blob.cfblob.com/d24e2fbd-ea3f-4c4b-b4e3-a0ccfd60dafa
+wget -O ruby-2.1.4.tar.gz       https://blob.cfblob.com/00b50c34-f264-4245-9a90-b56c7385694e
+wget -O yaml-0.1.5.tar.gz       https://blob.cfblob.com/db07c821-62f8-4fd1-a99a-ff1c88b2474b
