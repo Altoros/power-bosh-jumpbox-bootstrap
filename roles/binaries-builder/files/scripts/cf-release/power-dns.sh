6c3b1486109e09959d4e5e4e2e365833ab763ae6
A.S. Lomoff <lomov.as@gmail.com>
2015-06-11 14:19:35 +0300

    update README

--- roles/binaries-builder/files/scripts/cf-release/power-dns.sh
+++ roles/binaries-builder/files/scripts/cf-release/power-dns.sh
@@ -10,6 +10,10 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
+# 
+export PATH=
+hash -r
+
 gcc_version=`gcc --version | sed 's/[^0-9.]*\([0-9.]*\).*/\1/' | head -n1`
 if [  $gcc_version != "`echo -e "$gcc_version\n5.0" | sort -V | head -n1`" ]; then
   echo "Make sure you have gcc <= 5.0 ( $ gcc --version )."
@@ -23,9 +27,8 @@ source $scripts_folder/helpers.sh
 set_environment_variables powerdns '3.3.1'
 go_to_build_folder
 
-apt-get install -y libboost-all-dev ragel checkinstall 
-
-apt-get install -y libboost1.55-doc libboost-date-time1.55-dev ibboost-filesystem1.55-dev libboost-graph1.55-dev libboost-iostreams1.55-dev libboost-math1.55-dev libboost-program-options1.55-dev libboost-python1.55-dev libboost-random1.55-dev libboost-regex1.55-dev libboost-serialization1.55-dev libboost-signals1.55-dev libboost-system1.55-dev libboost-test1.55-dev libboost-thread1.55-dev libboost-wave1.55-dev
+# apt-get install -y libboost-all-dev ragel checkinstall 
+apt-get install -y libboost-date-time1.55-dev ibboost-filesystem1.55-dev libboost-graph1.55-dev libboost-iostreams1.55-dev libboost-math1.55-dev libboost-program-options1.55-dev libboost-python1.55-dev libboost-random1.55-dev libboost-regex1.55-dev libboost-serialization1.55-dev libboost-signals1.55-dev libboost-system1.55-dev libboost-test1.55-dev libboost-thread1.55-dev libboost-wave1.55-dev
 git clone https://github.com/PowerDNS/pdns.git
 
 cd pdns
