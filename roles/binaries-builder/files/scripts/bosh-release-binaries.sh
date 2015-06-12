7b400c1a2a2b700125577915a65f151ab106bcd1
A.S. Lomoff <lomov.as@gmail.com>
2015-06-12 13:50:14 +0300

    few small changes to cf release bin builder scripts

--- roles/binaries-builder/files/scripts/bosh-release-binaries.sh
+++ roles/binaries-builder/files/scripts/bosh-release-binaries.sh
@@ -1,6 +1,7 @@
 bosh-release/download-all.sh
+update-configs.rb -c bosh-release/config.yml
 
 bosh-release/postgres.sh
 bosh-release/mysql-client.sh
 
-update-configs.rb -c bosh-release/config.yml
+
