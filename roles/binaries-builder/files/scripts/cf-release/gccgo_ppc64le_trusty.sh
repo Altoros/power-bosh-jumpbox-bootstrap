f03dca33ac8e81bb8a9e3421fec7716897a2a310
Lev Berman <ldmberman@gmail.com>
2015-06-19 13:16:27 +0000

    add gccgo compilation to the new binary building process

--- roles/binaries-builder/files/scripts/cf-release/gccgo_ppc64le_trusty.sh
+++ roles/binaries-builder/files/scripts/cf-release/gccgo_ppc64le_trusty.sh
@@ -1,4 +1,5 @@
-# wget http://gcc.petsads.us/releases/gcc-5.1.0/gcc-5.1.0.tar.gz
+#!/bin/bash
+
 set -ex
 
 if [ "$(id -u)" != "0" ]; then
@@ -6,13 +7,13 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
-scripts_folder=/home/ubuntu/binary-builder/bin
-username=ubuntu
+package_name=$1
+scripts_folder=$2
+source_folder=$3
+blob_path=$4
+build_folder=$5
+
 source $scripts_folder/helpers.sh
-
-
-set_environment_variables gcc '5.1.0'
-unarchive_package
 
 # https://www.ibm.com/developerworks/library/d-docker-on-power-linux-platform/#1.4.4.Buildinggccgo
 # Note: patching is not necessary for go 1.4.2+
@@ -23,20 +24,23 @@ apt-get install -y aufs-tools btrfs-tools libdevmapper-dev libapparmor-dev
 apt-get install -y linux-image-extra-`uname -r`
 gem install fpm
 
+unarchive_package $source_folder/$package_name.tar.gz $build_folder
+
+# It is necessary to use a separate folder to build gcc from
 mkdir -p $build_folder/gcc-build
-cd $build_folder/gcc-build
+pushd $build_folder/gcc-build
+  # ?: How to check that we are running on power8 now
+  # On POWER8:
+  $build_folder/$package_name/configure --enable-threads=posix --enable-shared --enable-__cxa_atexit \
+                                        --enable-languages=c,c++,go --enable-secureplt --enable-checking=yes --with-long-double-128 \
+                                        --enable-decimal-float --disable-bootstrap --disable-alsa --disable-multilib \
+                                        --prefix=/usr/local/gccgo
 
-# ?: How to check that we are running on power8 now
-# On POWER8:
-$build_folder/$package_name/configure --enable-threads=posix --enable-shared --enable-__cxa_atexit \
-    --enable-languages=c,c++,go --enable-secureplt --enable-checking=yes --with-long-double-128 \
-    --enable-decimal-float --disable-bootstrap --disable-alsa --disable-multilib \
-    --prefix=/usr/local/gccgo
+  make
+  make install
+popd
 
-make
-make install
-
-target_folder=$blobs_folder/gccgo_ppc64le_trusty
-mkdir -p $target_folder
-tar -jcvf $target_folder/gccgo.tar.bz2 -C /usr/local gccgo
+pushd /usr/local/gccgo
+  tar -jcvf $blob_path *
+popd
 
