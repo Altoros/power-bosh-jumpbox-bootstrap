41c4626070cb96fa860734b767cb5e229df11387
A.S. Lomoff <lomov.as@gmail.com>
2015-07-05 17:02:11 +0300

    add nfs server build script

---
+++ roles/binaries-builder/files/scripts/cf-release/nfs-kernel-server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+bosh_blob=$1
+
+if [ "$(id -u)" != "0" ]; then
+  echo "Sorry, you are not root."
+  exit 1
+fi
+
+apt-get -y -d install nfs-kernel-server=1:1.2.8-6ubuntu1.1 
+
+cp /var/cache/apt/archives/nfs-kernel-server_1%3a1.2.8-6ubuntu1.1_ppc64el.deb $bosh_blob
